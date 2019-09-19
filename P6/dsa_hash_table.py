ad2ba9040fbaa90d8217f7ef6e839bf447a7389c
Reece Jones (DESKTOP UNT4J9U) <reece.jones131@gmail.com>
2019-09-20 00:03:56 +0800

    P6: more graph impl and tests.

--- P6/dsa_hash_table.py
+++ P6/dsa_hash_table.py
@@ -10,14 +10,6 @@ __all__ = [
 ]
 
 
-def _is_prime(x: int) -> bool:
-    res = True
-    for i in takewhile(lambda _: res, range(3, ceil(sqrt(x)))):
-        if x % i == 0:
-            res = False
-    return res
-
-
 class DSAHashTable:
     class _Entry:
         NEVER_USED = 0
@@ -29,6 +21,7 @@ class DSAHashTable:
             self.key = None
             self.value = None
 
+        # For debugging purposes only.
         def __repr__(self) -> str:
             if self.state == self.USED:
                 res = f"{self.key}, {self.value}"
@@ -36,13 +29,18 @@ class DSAHashTable:
                 res = "NEVER_USED"
             elif self.state == self.PREV_USED:
                 res = "PREV_USED"
+            else:
+                raise AssertionError(f"Didn't expect state `{self.state}`.")
             return res
 
     MAX_LOAD_FACTOR = 0.5
-    MIN_GROWTH = 10
+    MIN_GROWTH = 20
 
-    def __init__(self, size: int) -> None:
-        self._array = numpy.empty(size, dtype=numpy.object)
+    def __init__(self, capacity: int) -> None:
+        if capacity < 1:
+            raise ValueError(f"capacity must be >=1, got {capacity}.")
+        capacity = self._next_prime(capacity)
+        self._array = numpy.empty(capacity, dtype=numpy.object)
         for i in range(self.capacity):
             self._array[i] = self._Entry()
         self._used = 0
@@ -50,25 +48,16 @@ class DSAHashTable:
     def put(self, key: str, value: Any) -> None:
         self._put(self._array, key, value)
         self._used += 1
-
         if self.load_factor >= self.MAX_LOAD_FACTOR:
             self._grow_array()
 
     def get(self, key: str) -> Any:
-        idx = DSAHashTable._hash(key) % self.capacity
-        original_idx = idx
-        found = False
-        while not found:
-            entry = self._array[idx]
-            if entry.state == self._Entry.USED and entry.key == key:
-                res = entry.value
-                found = True
-            else:
-                if entry.state in (self._Entry.USED, self._Entry.PREV_USED):
-                    idx = (idx + 1) % self.capacity
-                if entry.state == self._Entry.NEVER_USED or idx == original_idx:
-                    raise KeyError(f"Key `{key}` not in hash table.")
-        return res
+        return self._get(key).value
+
+    def delete(self, key: str) -> None:
+        entry = self._get(key)
+        entry.state = self._Entry.PREV_USED
+        self._used -= 1
 
     def has(self, key: str) -> bool:
         try:
@@ -79,15 +68,36 @@ class DSAHashTable:
         return res
 
     @property
+    def size(self) -> int:
+        return self._used
+
+    @property
     def load_factor(self) -> float:
-        return self._used / self.capacity
+        return self.size / self.capacity
 
     @property
     def capacity(self) -> int:
         return self._array.size
 
+    def _get(self, key: str) -> _Entry:
+        idx = self._hash(key, self.capacity)
+        step = DSAHashTable._step_hash(key, self.capacity)
+        original_idx = idx
+        found = False
+        while not found:
+            entry = self._array[idx]
+            if entry.state == self._Entry.USED and entry.key == key:
+                res = entry
+                found = True
+            else:
+                if entry.state in (self._Entry.USED, self._Entry.PREV_USED):
+                    idx = (idx + step) % self.capacity
+                if entry.state == self._Entry.NEVER_USED or idx == original_idx:
+                    raise KeyError(f"Key `{key}` not in hash table.")
+        return res
+
     def _grow_array(self) -> None:
-        new_size = next(filter(_is_prime, count(self.capacity + self.MIN_GROWTH)))
+        new_size = self._next_prime(self.capacity + self.MIN_GROWTH)
         new_array = numpy.empty(new_size, dtype=numpy.object)
         for i in range(new_size):
             new_array[i] = self._Entry()
@@ -97,18 +107,50 @@ class DSAHashTable:
         self._array = new_array
 
     @staticmethod
-    def _hash(key: str) -> int:
+    def _put(array: numpy.ndarray, key: str, value: Any) -> None:
+        idx = DSAHashTable._hash(key, array.size)
+        step = DSAHashTable._step_hash(key, array.size)
+        # Assumes array never becomes 100% full.
+        done = False
+        while not done:
+            entry = array[idx]
+            free = entry.state != DSAHashTable._Entry.USED
+            if free or entry.key == key:
+                if free:
+                    entry.key = key
+                    entry.state = DSAHashTable._Entry.USED
+                entry.value = value
+                done = True
+            else:
+                idx = (idx + step) % array.size
+
+    @staticmethod
+    def _hash(key: str, array_size: int) -> int:
         res = 0
         for c in key:
             res = 33 * res + ord(c)
-        return res
+        return res % array_size
 
     @staticmethod
-    def _put(array: numpy.ndarray, key: str, value: Any) -> None:
-        idx = DSAHashTable._hash(key) % array.size
-        # Assumes array never becomes 100% full.
-        while array[idx].state == DSAHashTable._Entry.USED:
-            idx = (idx + 1) % array.size
-        array[idx].key = key
-        array[idx].value = value
-        array[idx].state = DSAHashTable._Entry.USED
+    def _step_hash(key: str, array_size: int) -> int:
+        return max(1, (sum(map(ord, key)) + len(key)) % array_size)
+
+    @staticmethod
+    def _next_prime(x: int) -> int:
+        def _is_prime(x: int) -> bool:
+            assert x > 2
+            res = True
+            for i in takewhile(lambda _: res, range(2, 1 + ceil(sqrt(x)))):
+                if x % i == 0:
+                    res = False
+            return res
+
+        assert x >= 1
+        if x == 1:
+            res = 2
+        else:
+            x += 1
+            if x % 2 == 0:
+                x += 1
+            res = next(filter(_is_prime, count(x, 2)))
+        return res
--- P6/dsa_hash_table_test.py
+++ P6/dsa_hash_table_test.py
@@ -11,51 +11,108 @@ def random_string(max_size: int = 20) -> str:
     for i in range(size):
         # Random Unicode character.
         res += chr(random.randint(0, 1114111))
+    assert 0 < len(res) <= max_size
     return res
 
 
 class DSAHashTableTest(unittest.TestCase):
-    TEST_SIZE = 10
+    TEST_SIZE = 2500
 
     def setUp(self) -> None:
         self._hashtable = DSAHashTable(self.TEST_SIZE)
 
-    def test_put_get(self) -> None:
-        # Add key, value pairs (unique keys).
-        keys = self.unique_keys()
+    def test_next_prime(self) -> None:
+        # Assert next prime from non-prime is correct.
+        for x, y in [(1, 2), (4, 5), (6, 7), (8, 11), (9, 11), (10, 11), (12, 13), (14, 17), (15, 17), (16, 17), (18, 19)]:
+            self.assertEqual(y, DSAHashTable._next_prime(x))
+
+        # Assert next prime from prime is correct.
+        for x, y in [(2, 3), (3, 5), (5, 7), (7, 11), (11, 13), (13, 17), (17, 19)]:
+            self.assertEqual(y, DSAHashTable._next_prime(x))
+
+    def test_put_get_has(self) -> None:
+        keys = self.unique_keys(self.TEST_SIZE)
+
+        # Assert nothing in table yet.
+        for key in keys:
+            with self.assertRaises(KeyError):
+                self._hashtable.get(key)
+            self.assertFalse(self._hashtable.has(key))
+
+        # Add key, value pairs.
         d = {}
         for key in keys:
             value = random.choice(range(self.TEST_SIZE))
             self._hashtable.put(key, value)
-            # Assert keys map to correct values before internal resize occurs.
+            # Assert keys map to correct values before any internal resize occurs.
             self.assertEqual(value, self._hashtable.get(key))
+            self.assertTrue(self._hashtable.has(key))
             d[key] = value
 
         # Assert keys still map to correct values after internal resize.
         random.shuffle(keys)
         for key in keys:
             self.assertEqual(d[key], self._hashtable.get(key))
+            self.assertTrue(self._hashtable.has(key))
 
         # Change some values of existing keys.
         for key in random.sample(keys, self.TEST_SIZE // 2):
             d[key] += self.TEST_SIZE
             self._hashtable.put(key, d[key])
             self.assertEqual(d[key], self._hashtable.get(key))
+            self.assertTrue(self._hashtable.has(key))
 
         # Assert keys still map to correct values after all changes.
         random.shuffle(keys)
         for key in keys:
             self.assertEqual(d[key], self._hashtable.get(key))
+            self.assertTrue(self._hashtable.has(key))
 
+    def test_delete(self) -> None:
+        keys = self.unique_keys(self.TEST_SIZE)
+
+        # Assert can't delete non-existent keys.
+        for key in keys:
+            with self.assertRaises(KeyError):
+                self._hashtable.delete(key)
+
+        # Add key, value pairs.
+        for key in keys:
+            value = random.choice(range(self.TEST_SIZE))
+            self._hashtable.put(key, value)
+
+        # Delete entries and assert they got removed.
+        random.shuffle(keys)
+        for key in keys:
+            self._hashtable.delete(key)
+            self.assertFalse(self._hashtable.has(key))
+            with self.assertRaises(KeyError):
+                self._hashtable.get(key)
+
+        # Assert entries stayed removed while removing others.
+        random.shuffle(keys)
+        for key in keys:
+            self.assertFalse(self._hashtable.has(key))
+            with self.assertRaises(KeyError):
+                self._hashtable.get(key)
+
+        # Assert can't re-delete entries.
+        random.shuffle(keys)
+        for key in keys:
+            with self.assertRaises(KeyError):
+                self._hashtable.delete(key)
 
     @staticmethod
-    def unique_keys() -> List[str]:
+    def unique_keys(count: int) -> List[str]:
+        key_set = set()
         keys = []
-        for _ in range(DSAHashTableTest.TEST_SIZE):
+        for _ in range(count):
             key = random_string()
-            while key in keys:
+            while key in key_set:
                 key = random_string()
             keys.append(key)
+            key_set.add(key)
+        assert len(keys) == count
         return keys
 
 
